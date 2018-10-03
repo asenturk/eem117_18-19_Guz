c392099fe37e0551a8af812227375565d655720d
Ali Senturk <asntrk@gmail.com>
2018-10-03 13:27:49 +0300

    1. ogretim ornekler eklendi

---
+++ ornekler/eem117_ders01_1o.c
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+
+	cout << "Merhaba Dunya"<< endl;
+	cout << " Bu benim ilk programim";
+	cout << endl << "Merhaba dunya" << endl << "Bu benim ilk programim" <<endl;
+
+	system("pause");
+	return 0;
+}
+
+_____________
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+	int a;
+	int b;
+	int c;
+	a = 1;
+	b = 2;
+	c = a + b;
+	cout << "birinci sayi: " << a << endl;
+	cout << "ikinci sayi: " << b << endl;
+	cout << "sayilarin toplami: " << c << endl;
+	
+	system("pause");
+	return 0;
+}
+
+____________
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+	int a=1, b=2, c;
+	c = a + b;
+	cout << "birinci sayi: " << a << endl;
+	cout << "ikinci sayi: " << b << endl;
+	cout << "sayilarin toplami: " << c << endl;
+	
+	system("pause");
+	return 0;
+}
+
+_____________
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+	int a, b, c;
+	cout << "toplama islemi icin birinci sayiyi giriniz";
+	cin >> a;
+	cout << "toplama islemi icin ikinci sayiyi giriniz";
+	cin >> b;
+	c = a + b;
+	cout << "birinci sayi: " << a << endl;
+	cout << "ikinci sayi: " << b << endl;
+	cout << "sayilarin toplami: " << c << endl;
+	
+	system("pause");
+	return 0;
+}
