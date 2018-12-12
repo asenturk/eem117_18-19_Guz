19e9a682363b2bdaa84f4fae729e7730befce23d
Ali Senturk <asntrk@gmail.com>
2018-12-12 23:07:56 +0300

    Fonksiyonların farklı durumlarını anlatan Prof. Dr. Tuncay AYDOĞAN'ın örnekleri.

---
+++ ornekler/eem117_ders09_ek.cpp
@@ -0,0 +1,89 @@
+//parametresiz fonksiyon örneði
+#include <iostream>
+
+using namespace std;
+
+void girhesaplaveyaz(void)
+{
+int a, b,c;    
+cout<<"birinci sayiyi giriniz:";
+cin>>a;    
+cout<<"ikinci sayiyi giriniz:";
+cin>>b;
+
+c=a+b;
+cout<<c;    
+}
+
+int main (void)
+{ 
+girhesaplaveyaz();           
+system("pause");
+return 1;
+}
+
+
+___________
+
+
+//parametre alan fonksiyon örneði
+#include <iostream>
+
+using namespace std;
+
+void alhesaplaveyaz(int s1,int s2)
+{
+int c;
+c=s1+s2;
+cout<<c;    
+}
+
+int main (void)
+{ 
+int a, b;   
+cout<<"birinci sayiyi giriniz:";
+cin>>a;    
+cout<<"ikinci sayiyi giriniz:";
+cin>>b;
+
+alhesaplaveyaz(a,b);
+            
+system("pause");
+return 1;
+}
+
+
+___________
+
+
+//parametre alan ve sonuç gönderen fonksiyon örneði
+#include <iostream>
+
+using namespace std;
+
+int alhesaplavegonder(int s1,int s2)
+{
+int sonuc;
+sonuc=s1+s2;
+return sonuc;   
+}
+
+int main (void)
+{ 
+int a, b,c;   
+cout<<"birinci sayiyi giriniz:";
+cin>>a;    
+cout<<"ikinci sayiyi giriniz:";
+cin>>b;
+
+//fonksiyondan gelen sonucun bir deðiþkene alýnarak kullanýlmasý
+c=alhesaplavegonder(a,b);
+cout<<c;          
+
+//fonksiyondan gelen sonucun doðrudan ekrana yazdýrýlmasý
+cout<<alhesaplavegonder(a,b);
+   
+system("pause");
+return 1;
+}
+
